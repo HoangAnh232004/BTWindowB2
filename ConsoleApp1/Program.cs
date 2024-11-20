b3d7c32e2ab33e66175c917550cb84db9ff0ed36
84345 <84345@MSI>
2024-11-20 16:26:12 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+        static void PrintAllStudents(List<Student> students)
+        {
+            Console.WriteLine("Danh sach toan bo hoc sinh");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            }
+            Console.WriteLine();
+        }
+        static void PrintStudentsByAgeRange(List<Student> students, int minAge, int maxAge)
+        {
+            Console.WriteLine($"Hoc sinh co tuoi tu {minAge} den {maxAge}:");
+            var filteredStudents = students.Where(s => s.Age >= minAge && s.Age <= maxAge).ToList();
+            foreach (var student in filteredStudents)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            }
+            Console.WriteLine();
+        }
+        static void PrintStudentsByNameStartWith(List<Student> students, string startWith)
+        {
+            Console.WriteLine($"Hoc sinh co chu bat dau bang '{startWith}':");
+            var filteredStudents = students.Where(s => s.Name.StartsWith(startWith, StringComparison.OrdinalIgnoreCase)).ToList();
+            foreach (var student in filteredStudents)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            }
+            Console.WriteLine();
+        }
+        static void PrintTotalAge(List<Student> students)
+        {
+            int totalAge = students.Sum(s => s.Age);
+            Console.WriteLine($"Tong so tuoi cua tat ca hoc sinh: {totalAge}");
+            Console.WriteLine();
+        }
+        static void PrintOldestStudent(List<Student> students)
+        {
+            var oldestStudent = students.OrderByDescending(s => s.Age).FirstOrDefault();
+            if (oldestStudent != null)
+            {
+                Console.WriteLine("Hoc sinh co tuoi lon nhat:");
+                Console.WriteLine($"Name: {oldestStudent.Name}, Age: {oldestStudent.Age}");
+            }
+            Console.WriteLine();
+        }
+        static void PrintStudentsSortedByAge(List<Student> students)
+        {
+            Console.WriteLine("Danh sach hoc sinh sau khi sap xep tuoi tang dan:");
+            var sortedStudents = students.OrderBy(s => s.Age).ToList();
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args) {
+         List<Student> students = new List<Student>
+        {
+            new Student { Name = "Anh", Age = 20 },
+            new Student { Name = "Bao", Age = 16 },
+            new Student { Name = "An", Age = 18 },
+            new Student { Name = "Cuong", Age = 14 },
+            new Student { Name = "Duong", Age = 19 }
+        };
+
+        PrintAllStudents(students);                
+        PrintStudentsByAgeRange(students, 15, 18); 
+        PrintStudentsByNameStartWith(students, "A");
+        PrintTotalAge(students);                  
+        PrintOldestStudent(students);             
+        PrintStudentsSortedByAge(students);       
+    }
+}
+
+    }
+
+
---
+++ ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cc4093e-cc9a-40e5-9e03-07d1bb4e7c24")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
